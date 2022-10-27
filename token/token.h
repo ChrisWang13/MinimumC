64b491c32ab360e65720f10c68b9a47721090736
ChrisWang13 <chrisqq13@gmail.com>
2022-10-27 16:38:55 +0800

    Add struct Obj and Node.

--- token/token.h
+++ token/token.h
@@ -40,8 +40,50 @@ void error_tok(struct Token *tok, char *fmt, ...);
 
 // Util functions
 bool is_match(struct Token *tok, const char *op);
+bool cur_token(struct Token **rest, struct Token *tok, char *str);
+struct Token *next_token(struct Token *tok, char *op);
 
 // Called in main function
 struct Token *split_token(char *input);
+
+//
+// parse.c
+//
+typedef struct Type Type;
+typedef struct Node Node;
+
+// Variable or function
+typedef struct Obj Obj;
+struct Obj {
+  Obj *next;
+  char *name;       // Variable name
+  bool is_local;    // local or global/function
+  int offset;       // Local variable
+  bool is_function; // Global variable or function
+};
+
+// AST node kind, differ from token
+typedef enum {
+  ND_ADD,       // +
+  ND_SUB,       // -
+  ND_MUL,       // *
+  ND_DIV,       // /
+  ND_RETURN,    // "return"
+  ND_VAR,       // Variable
+  ND_NUM,       // Integer
+} NodeKind;
+
+// AST node type
+struct Node {
+  NodeKind kind; // Node kind
+  Node *next;    // Next node
+  struct Token *tok;    // Representative token
+
+  Node *lhs;     // Left-hand side
+  Node *rhs;     // Right-hand side
+
+  Obj *var;      // Used if kind == ND_VAR
+  int val;       // Used if kind == ND_NUM
+};
 
 #endif // End of _TOKEN_H_
